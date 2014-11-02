6c5cf9a83ea4550cd940fd50a592d42059a7c4ee
Justin Jones <justin@Justins-MacBook-Pro.local>
2014-11-02 14:45:19 -0500

    comment for Sublime Package Control

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -6,4 +6,5 @@ brew install bash-completion
 brew install hub
 
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
-
+# Sublime Package Control
+# https://sublime.wbond.net/installation
