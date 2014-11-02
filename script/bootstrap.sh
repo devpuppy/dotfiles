25d0b9cf87add30aa2de581b1e41811afe539b60
Justin Jones <justin@Justins-MacBook-Pro.local>
2014-11-02 15:19:42 -0500

    install rbenv

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -5,6 +5,9 @@ brew install git
 brew install bash-completion
 brew install hub
 
+brew install rbenv ruby-build
+
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
 # Sublime Package Control
 # https://sublime.wbond.net/installation
