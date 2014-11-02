7884bfefcd1f5de706602a2ccf22e9aafa70edef
Justin Jones <justin@devpuppy.com>
2014-11-02 14:30:47 -0500

    Create bootstrap.sh

---
+++ script/bootstrap.sh
@@ -0,0 +1,9 @@
+
+# install homebrew
+
+brew install git
+brew install bash-completion
+brew install hub
+
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
