578587c3e506af391fd5636aab0f1286e1bfdb60
Justin Jones <justin@devpuppy.com>
2015-08-21 13:53:17 -0400

    install readline and compile rbenv ruby against it

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,4 +1,3 @@
-
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -6,9 +5,10 @@ brew install bash-completion git hub
 
 brew install rbenv ruby-build rbenv-binstubs
 
+brew install readline
+# $ RUBY_CONFIGURE_OPTS=--with-readline-dir=`brew --prefix readline` rbenv install 2.2.2
+
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
 # Sublime Package Control
 # https://sublime.wbond.net/installation
-
-
