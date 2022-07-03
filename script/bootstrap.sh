dec6d660202dd0228cc54c2f2ea311c20a9e8811
Justin Jones <justin@devpuppy.com>
2022-07-03 13:11:17 -0400

    Update bootstrap.sh
    homebrew install bash

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,5 +1,5 @@
 # install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install bash-completion git hub
 
